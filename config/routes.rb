73a9d215b61f0e3c673f98fd63d138b41922b648
Federico Bucchi <federico.bucchi@gonitro.com>
2015-05-25 22:23:32 -0700

    add create user action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-      resources :users, :only => [:show], defaults: {format: :json}
+      resources :users, :only => [:show, :create], defaults: {format: :json}
 
 end
