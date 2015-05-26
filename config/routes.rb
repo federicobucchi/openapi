c2d7c283028a86b659529f1c963c41c5a81f1e37
Federico Bucchi <federico.bucchi@gonitro.com>
2015-05-25 23:01:29 -0700

    add destroy user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-      resources :users, :only => [:show, :create], defaults: {format: :json}
+      resources :users, :only => [:show, :create, :update, :destroy], defaults: {format: :json}
 
 end
